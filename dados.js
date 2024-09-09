713ce497046b097ab0d38da6efa962cdc54b4822
amigof10 <fernandaamigops@gmail.com>
2024-09-09 15:49:16 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+{
+titulo : "Evolução",
+descricao : " evolução é o processo gradual de transformação das espécies ao longo de milhares de anos. Imagine uma árvore genealógica gigante, onde cada galho representa uma espécie diferente. Todas essas espécies, desde as bactérias até os seres humanos, compartilham um ancestral comum e foram se diversificando ao longo do tempo. As principais forças que impulsionam a evolução são as mutações (alterações no DNA), a seleção natural (onde os indivíduos mais adaptados ao ambiente têm mais chances de sobreviver e se reproduzir) e a adaptação (modificações que permitem aos organismos sobreviverem em diferentes ambientes).",
+link : "https://evosite.ib.usp.br/",
+tags : "Darwin Lamark Lamarkismo Darwinismo" 
+},
+{
+titulo : "As leis de Mendel",
+descricao : "As Leis de Mendel são um conjunto de princípios básicos da hereditariedade, estabelecidas por Gregor Mendel através de seus experimentos com ervilhas. Elas explicam como as características são transmitidas de uma geração para outra.",
+link : "https://www.todamateria.com.br/leis-de-mendel/",
+tags : "genética ervilhas"
+},
+{
+titulo : "Divisão celular",
+descricao : "A divisão celular é o processo pelo qual uma célula se divide em duas ou mais células filhas. Existem dois tipos principais de divisão celular: a mitose e a meiose, cada uma com funções específicas no organismo.",
+link : "https://brasilescola.uol.com.br/biologia/divisao-celular.htm",
+tags : "mitose meiose"
+}
+];
